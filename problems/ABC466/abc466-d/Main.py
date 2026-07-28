0385c8db55f86d5ae48dc70e82abc89e6290a6a5
Kanayan <glglntn6@gmail.com>
2026-07-29 00:22:09 +0900

    abc46 6-d 復習

--- problems/ABC466/abc466-d/Main.py
+++ problems/ABC466/abc466-d/Main.py
@@ -1,20 +1,39 @@
+# MyCode
+# n, m = map(int, input().split())
+# matrix = [[0] * n for _ in range(n)]
+
+# for _ in range(m):
+#     r, c = map(int, input().split())
+
+#     # r行初期化
+#     matrix[r - 1] = [0] * n
+
+#     # c列初期化
+#     for row in matrix:
+#         row[c - 1] = 0
+
+#     # コマを置く
+#     matrix[r - 1][c - 1] = 1
+
+# count = sum(1 for row in matrix for x in row if x == 1)
+
+# print(count)
+
+# 逆捜査式コード
 n, m = map(int, input().split())
-matrix = [[0] * n for _ in range(n)]
+opes = [tuple(map(int, input().split())) for _ in range(m)]
 
-for _ in range(m):
-    r, c = map(int, input().split())
+# 使用済み列番号
+used_rows = set()
 
-    # r行初期化
-    matrix[r - 1] = [0] * n
+# 使用済み行番号
+used_cols = set()
 
-    # c列初期化
-    for row in matrix:
-        row[c - 1] = 0
+ans = 0
+for r, c in reversed(opes):
+    if r not in used_rows and c not in used_cols:
+        ans = 0
+    used_cols.add(r)
+    used_cols.add(c)
 
-    # コマを置く
-    matrix[r - 1][c - 1] = 1
-
-count = sum(1 for row in matrix for x in row if x == 1)
-
-# print(matrix)
-print(count)
+print(ans)
